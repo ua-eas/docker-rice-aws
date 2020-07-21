584c77950c458bf9488ceb76a0e9efe3b53642f5
Heather Lo <hlo@email.arizona.edu>
2020-07-21 10:47:00 -0700

    FIN-1586 Undoing ojdbc version change made for FIN-1585

--- bin/liquibase_rice.sh
+++ bin/liquibase_rice.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # liquibase_rice.sh: main liquibase script based on liquibase_kfs.sh
 # Calls liquibase with correct database parameters
-# for this environment, path to ojdbc8-19c.jar file.
+# for this environment, path to ojdbc6.jar file.
 
 RICE_CONFIG_FILE=$RICE_CONFIG_DIRECTORY/rice-config.xml
 
@@ -14,7 +14,7 @@ exec /usr/bin/java -jar $LIQUIBASE_HOME/liquibase.jar \
 --url="$LIQUIBASE_DB_URL" \
 --username=$LIQUIBASE_DB_USERNAME \
 --password=$LIQUIBASE_DB_PASSWORD \
---classpath=$TOMCAT_SHARE_LIB/ojdbc8-19c.jar \
+--classpath=$TOMCAT_SHARE_LIB/ojdbc6.jar \
 --driver=oracle.jdbc.driver.OracleDriver \
 --logLevel=info \
 $@
